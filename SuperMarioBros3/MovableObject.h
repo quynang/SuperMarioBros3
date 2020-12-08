380d6ae115da0a884b17a20350ca915aeb069677
nang <quynang2014@gmail.com>
2020-12-08 23:31:57 +0700

    Implemetation of AddCoObjects method for MovableObject class

--- SuperMarioBros3/MovableObject.h
+++ SuperMarioBros3/MovableObject.h
@@ -1,5 +1,17 @@
 #pragma once
 #include "GameObject.h"
 class MovableObject : public CGameObject {
-	//TODO
+
+	void AddCoObjects(vector<LPGAMEOBJECT>* coObjects) {
+
+		CGameObject::AddCoObjects(coObjects);
+
+		for (size_t i = 0; i < coObjects->size(); i++) {
+			if (dynamic_cast<MovableObject*>(coObjects->at(i)))
+			{
+				MovableObject *m_obj = dynamic_cast<MovableObject *>(coObjects->at(i));
+				m_obj->AddCoObject(this);
+			}
+		}
+	}
 };
