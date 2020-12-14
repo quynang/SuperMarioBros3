d0f762dc393bd2ac00e31ee401706240e85a1a10
nang <quynang2014@gmail.com>
2020-12-15 03:39:50 +0700

    Exclude player from list object to check collision of all game object.

--- SuperMarioBros3/GameObject.cpp
+++ SuperMarioBros3/GameObject.cpp
@@ -105,6 +105,9 @@ void CGameObject::FilterCollision(
 	{
 		LPCOLLISIONEVENT c = coEvents[i];
 
+		//Exclude player from list object to check collision of all game object. 
+		if (dynamic_cast<CMario*>(c->obj)) continue;
+
 		//TODO: Refactor here. Define list object we going to ignore proccessing collide by nx.
 		if (c->t < min_tx && c->nx != 0 && !dynamic_cast<CBigBox *>(c->obj) && !dynamic_cast<CGround *>(c->obj)) {
 			min_tx = c->t; nx = c->nx; min_ix = i; rdx = c->dx;
